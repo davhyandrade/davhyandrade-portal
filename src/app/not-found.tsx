3ad8517a28ae58aeaee07d63444eee5c47d49c3b
davhyandrade <davhyhello@gmail.com>
2026-03-10 21:14:51 -0300

    feat: adds not found file

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,13 +1,48 @@
-import Link from "next/link";
+import { Button, Stack, Typography } from "@mui/material";
 
-function NotFound() {
+function NotFoundPage() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <Stack
+      width="100%"
+      height="100dvh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Stack alignItems="center" padding={2}>
+        <Typography variant="h1" color="text.primary">
+          404
+        </Typography>
+
+        <Typography
+          variant="body1"
+          color="text.primary"
+          textAlign="center"
+          fontWeight={600}
+        >
+          Página não encontrada
+        </Typography>
+
+        <Typography
+          variant="body2"
+          color="text.secondary"
+          maxWidth={300}
+          textAlign="center"
+          mt={2}
+        >
+          Ela pode ter sido removida ou o endereço está incorreto.
+        </Typography>
+
+        <Button
+          variant="text"
+          color="primary"
+          href="/"
+          sx={{ mt: 8, fontWeight: 600 }}
+        >
+          Voltar para a home
+        </Button>
+      </Stack>
+    </Stack>
   );
 }
 
-export default NotFound;
+export default NotFoundPage;
