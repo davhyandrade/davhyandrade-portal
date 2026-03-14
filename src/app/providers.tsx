b1c52da3fd4e4e65dd07a0b73fd9beaf530ced69
davhyandrade <davhyhello@gmail.com>
2026-03-14 16:52:49 -0300

    feat(MUI): adds theme config

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,16 @@
+"use client";
+
 import type { ReactNode } from "react";
+import { ThemeProvider } from "@mui/material/styles";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
+import theme from "@/shared/lib/mui/theme/theme.config";
 
 function Providers({ children }: { children: ReactNode }) {
-  return children;
+  return (
+    <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </AppRouterCacheProvider>
+  );
 }
 
 export default Providers;
