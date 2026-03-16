419082022dd6d1cd664591617b2ba65d1fa11979
davhyandrade <davhyhello@gmail.com>
2026-03-16 18:26:40 -0300

    feat(Mui): adds custom styles to the link

--- src/shared/lib/mui/theme/theme.config.ts
+++ src/shared/lib/mui/theme/theme.config.ts
@@ -3,10 +3,15 @@ import { createTheme } from "@mui/material/styles";
 import breakpoints from "./breakpoints/breakpoints.config";
 import typography from "./typography/typography.config";
 
+import { MuiLink } from "./components/link/link.config";
+
 const theme = createTheme({
   spacing: 4,
   breakpoints,
   typography,
+  components: {
+    MuiLink,
+  },
 });
 
 export default theme;
