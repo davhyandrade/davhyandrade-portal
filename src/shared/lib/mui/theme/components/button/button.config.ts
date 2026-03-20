97ca082328023e266e8a86c2189d2e4afaa5c657
davhyandrade <davhyhello@gmail.com>
2026-03-19 21:02:29 -0300

    refact(button): changes default styles

--- src/shared/lib/mui/theme/components/button/button.config.ts
+++ src/shared/lib/mui/theme/components/button/button.config.ts
@@ -4,9 +4,10 @@ export const MuiButton: Components<Theme>["MuiButton"] = {
   styleOverrides: {
     root: ({ theme }) => ({
       fontSize: 16,
+      lineHeight: 1.4,
       textTransform: "none",
-      borderRadius: theme.spacing(3),
-      padding: theme.spacing(1, 6),
+      borderRadius: theme.spacing(4),
+      padding: theme.spacing(1.5, 5),
       transition: "color 0.3s, border-color 0.3s",
     }),
   },
