539f0ae2f3fe7999f33853fbecb2747b84b09011
davhyandrade <davhyhello@gmail.com>
2026-03-19 21:06:25 -0300

    feat(Mui): adds link component

---
+++ src/shared/lib/mui/components/link/Link.component.tsx
@@ -0,0 +1,14 @@
+import NextLink from "next/link";
+
+import { Link as MuiLink } from "@mui/material";
+import type { LinkProps } from "@mui/material";
+
+function Link({ href, children, ...rest }: LinkProps) {
+  return (
+    <MuiLink component={NextLink} href={href} {...rest}>
+      {children}
+    </MuiLink>
+  );
+}
+
+export default Link;
