3bab6856fa8e1986a7ad0cb730d25cfc00a55e12
davhyandrade <davhyhello@gmail.com>
2026-04-06 21:13:21 -0300

    chore: adds eslint plugin import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import prettier from 'eslint-config-prettier';
 import prettierPlugin from 'eslint-plugin-prettier';
 import jestPlugin from 'eslint-plugin-jest';
 import testingLibraryPlugin from 'eslint-plugin-testing-library';
+import importPlugin from 'eslint-plugin-import';
 
 const generalConfig = {
   rules: {
@@ -87,16 +88,43 @@ const jestConfig = [
       'jest/prefer-lowercase-title': 'error',
       'testing-library/prefer-user-event-setup': 'error',
       'testing-library/prefer-user-event': 'error',
+      'no-restricted-imports': 'off',
     },
   },
 ];
 
+const importConfig = {
+  ...importPlugin.flatConfigs.recommended,
+  files: ['**/*.ts', '**/*.tsx'],
+  plugins: {
+    import: importPlugin,
+  },
+  rules: {
+    ...importPlugin.flatConfigs.recommended.rules,
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
+  },
+};
+
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  ...jestConfig,
   generalConfig,
+  ...jestConfig,
   prettierConfig,
+  importConfig,
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
